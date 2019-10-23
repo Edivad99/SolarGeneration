65bfa3441292b63542d1e822ad2de7d99da3860e
Davide Albiero <davide2910@live.it>
2019-10-23 22:10:08 +0200

    Updated UI

--- src/main/java/edivad/solargeneration/gui/SolarPanelGui.java
+++ src/main/java/edivad/solargeneration/gui/SolarPanelGui.java
@@ -27,17 +27,17 @@ public class SolarPanelGui extends GuiContainer {
 		super.drawScreen(mouseX, mouseY, partialTicks);
 		renderHoveredToolTip(mouseX, mouseY);
 		if(mouseX > guiLeft + 7 && mouseX < guiLeft + 29 && mouseY > guiTop + 10 && mouseY < guiTop + 77)
-			drawHoveringText(Collections.singletonList("Energy: " + String.valueOf(getPercent()) + " %"), mouseX, mouseY, fontRenderer);
+			drawHoveringText(Collections.singletonList("Energy: " + String.valueOf(getPercent()) + "%"), mouseX, mouseY, fontRenderer);
 	}
 
 	@Override
 	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
 	{
 
-		String clientEnergy = "Stored enery: " + this.tileEntitySolarPanel.getClientEnergy() + " FE";
+		String clientEnergy = "Stored energy: " + getEnergyFormatted(this.tileEntitySolarPanel.getClientEnergy());
 		this.fontRenderer.drawString(clientEnergy, (this.xSize / 2 - this.fontRenderer.getStringWidth(clientEnergy) / 2) + 14, 20, 4210752);
 
-		String maxEnergy = "Max capacity: " + this.tileEntitySolarPanel.getMaxEnergy() + " FE";
+		String maxEnergy = "Max capacity: " + getEnergyFormatted(this.tileEntitySolarPanel.getMaxEnergy());
 		this.fontRenderer.drawString(maxEnergy, (this.xSize / 2 - this.fontRenderer.getStringWidth(maxEnergy) / 2) + 14, 30, 4210752);
 
 		String generation = "Generation: " + this.tileEntitySolarPanel.getClientCurrentAmountEnergyProduced() + " FE/t";
@@ -56,20 +56,27 @@ public class SolarPanelGui extends GuiContainer {
 		this.drawTexturedModalRect(this.guiLeft + 10, this.guiTop + 12 + y, 176, 0, 16, 60 - y);
 
 	}
-
+	
+	private String getEnergyFormatted(int energy)
+	{
+		if(energy >= 1000000)
+			return (energy / 1000) + " kFE";
+		else
+			return energy + " FE";
+	}
+	
 	private int getEnergyScaled(int pixels)
 	{
-		double percent = getPercent();
-		return pixels - (int) ((int) pixels * percent / 100);
+		return pixels - (pixels * getPercent() / 100);
 	}
 
-	private double getPercent()
+	private int getPercent()
 	{
-		int currentEnergy = this.tileEntitySolarPanel.getClientEnergy();
+		Long currentEnergy = new Long(this.tileEntitySolarPanel.getClientEnergy());
 		int maxEnergy = this.tileEntitySolarPanel.getMaxEnergy();
 
-		double result = currentEnergy * 100.0 / maxEnergy;
-		return result;
-	}
+		long result = currentEnergy * 100 / maxEnergy;
 
+		return (int) result;
+	}
 }
