740df4a8052ab940baf69558f2991079fa315d9e
Davide Albiero <davide2910@live.it>
2021-01-22 10:54:13 +0100

    2.4.0 Release

--- src/main/java/edivad/solargeneration/tools/SolarPanelLevel.java
+++ src/main/java/edivad/solargeneration/tools/SolarPanelLevel.java
@@ -6,16 +6,31 @@ public enum SolarPanelLevel {
 
     public String getSolarPanelName()
     {
-        return "solar_panel_" + this.name().toLowerCase();
+        return "solar_panel_" + getCorrectName(this.ordinal());
     }
 
     public String getSolarHelmetName()
     {
-        return "solar_helmet_" + this.name().toLowerCase();
+        return "solar_helmet_" + getCorrectName(this.ordinal());
     }
 
     public String getSolarCoreName()
     {
-        return "solar_core_" + this.name().toLowerCase();
+        return "solar_core_" + getCorrectName(this.ordinal());
+    }
+
+    private String getCorrectName(int index)
+    {
+        switch (index)
+        {
+            case 0: return "leadstone";
+            case 1: return "hardened";
+            case 2: return "redstone";
+            case 3: return "signalum";
+            case 4: return "resonant";
+            case 5: return "advanced";
+            case 6: return "ultimate";
+            default: throw new RuntimeException("Solar panel tier not yet implemented!");
+        }
     }
 }
