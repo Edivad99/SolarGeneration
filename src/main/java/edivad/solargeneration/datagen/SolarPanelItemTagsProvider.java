4a11af0e28ed07f5d2af3996cca571a47c7de066
Davide Albiero <davide2910@live.it>
2024-12-22 13:01:46 +0100

    Fix #32

--- src/main/java/edivad/solargeneration/datagen/SolarPanelItemTagsProvider.java
+++ src/main/java/edivad/solargeneration/datagen/SolarPanelItemTagsProvider.java
@@ -9,7 +9,9 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
 public class SolarPanelItemTagsProvider extends ItemTagsProvider {
@@ -29,5 +31,9 @@ public class SolarPanelItemTagsProvider extends ItemTagsProvider {
       this.tag(SolarGenerationTags.Items.SOLAR_HELMET)
           .add(Registration.HELMET.get(level).get());
     }
+    this.tag(Tags.Items.ARMORS)
+        .addTag(SolarGenerationTags.Items.SOLAR_HELMET);
+    this.tag(ItemTags.HEAD_ARMOR)
+        .addTag(SolarGenerationTags.Items.SOLAR_HELMET);
   }
 }
