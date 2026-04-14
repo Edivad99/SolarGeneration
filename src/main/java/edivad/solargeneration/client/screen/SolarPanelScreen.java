6281a0ed58be70f00027f27186c15af510865364
Davide Albiero <davide2910@live.it>
2026-04-14 20:34:45 +0200

    Port to 26.1.2

--- src/main/java/edivad/solargeneration/client/screen/SolarPanelScreen.java
+++ src/main/java/edivad/solargeneration/client/screen/SolarPanelScreen.java
@@ -5,7 +5,7 @@ import edivad.solargeneration.SolarGeneration;
 import edivad.solargeneration.blockentity.SolarPanelBlockEntity;
 import edivad.solargeneration.menu.SolarPanelMenu;
 import edivad.solargeneration.tools.Translations;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.client.gui.screens.inventory.tooltip.DefaultTooltipPositioner;
@@ -27,15 +27,13 @@ public class SolarPanelScreen extends AbstractContainerScreen<SolarPanelMenu> {
   }
 
   @Override
-  public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-    this.renderBackground(guiGraphics, mouseX, mouseY, partialTick);
-    super.render(guiGraphics, mouseX, mouseY, partialTick);
-    this.renderTooltip(guiGraphics, mouseX, mouseY);
+  public void extractRenderState(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float a) {
+    super.extractRenderState(graphics, mouseX, mouseY, a);
     if (mouseX > leftPos + 7 && mouseX < leftPos + 29 && mouseY > topPos + 10
         && mouseY < topPos + 77) {
       var component = Component.translatable(Translations.ENERGY, getPercent());
       var clienttooltipcomponent = ClientTooltipComponent.create(component.getVisualOrderText());
-      guiGraphics.renderTooltip(
+      graphics.tooltip(
           font,
           List.of(clienttooltipcomponent),
           mouseX, mouseY,
@@ -46,29 +44,31 @@ public class SolarPanelScreen extends AbstractContainerScreen<SolarPanelMenu> {
   }
 
   @Override
-  protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
+  protected void extractLabels(GuiGraphicsExtractor graphics, int xm, int ym) {
     var energy = Component.translatable(Translations.STORED_ENERGY,
         getEnergyFormatted(solarPanelBlockEntity.energyClient));
-    guiGraphics.drawString(font, energy, (imageWidth / 2 - font.width(energy) / 2) + 14, 20,
+    graphics.text(font, energy, (imageWidth / 2 - font.width(energy) / 2) + 14, 20,
         0xFF333333, false);
     var maxEnergy = Component.translatable(Translations.CAPACITY,
         getEnergyFormatted(solarPanelBlockEntity.getLevelSolarPanel().getCapacity()));
-    guiGraphics.drawString(font, maxEnergy, (imageWidth / 2 - font.width(maxEnergy) / 2) + 14, 30,
+    graphics.text(font, maxEnergy, (imageWidth / 2 - font.width(maxEnergy) / 2) + 14, 30,
         0xFF333333, false);
     var generation = Component.translatable(Translations.GENERATION,
         solarPanelBlockEntity.energyProductionClient);
-    guiGraphics.drawString(font, generation, (imageWidth / 2 - font.width(generation) / 2) + 14, 40,
+    graphics.text(font, generation, (imageWidth / 2 - font.width(generation) / 2) + 14, 40,
         0xFF333333, false);
   }
 
+
   @Override
-  protected void renderBg(GuiGraphics guiGraphics, float partialTicks, int mouseX, int mouseY) {
-    guiGraphics.blit(RenderPipelines.GUI_TEXTURED, TEXTURE, this.leftPos, this.topPos, 0, 0,
+  public void extractBackground(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float a) {
+    super.extractBackground(graphics, mouseX, mouseY, a);
+    graphics.blit(RenderPipelines.GUI_TEXTURED, TEXTURE, this.leftPos, this.topPos, 0, 0,
         this.imageWidth, this.imageHeight, 256, 256);
 
     // Energy
     int y = this.getEnergyScaled(60);
-    guiGraphics.blit(RenderPipelines.GUI_TEXTURED, TEXTURE, this.leftPos + 10, this.topPos + 12 + y,
+    graphics.blit(RenderPipelines.GUI_TEXTURED, TEXTURE, this.leftPos + 10, this.topPos + 12 + y,
         this.imageWidth, 0, 16, 60 - y, 256, 256);
   }
 
